51bdf0d99d2f6f03244aaf7af15b879e79f9bf54
Christian Harjuno <harjuno.tian@gmail.com>
2021-09-27 17:39:17 +0700

    Added token and id retrieval path

--- src/registry/getuserinfo.js
+++ src/registry/getuserinfo.js
@@ -1,3 +1,44 @@
+const { encryptUser, comparePassword } = require(__dirname + '/../../utils/encryption.js')
+const userData = require(__dirname + '/../../schema/userInfo.js')
+
+
 module.exports = async(req, res) =>{
-    
+    const { headers } = req
+    const username = headers.username
+    const password = headers.password
+    const userHash = await encryptUser(username)
+
+    const query = await userData.find({username: userHash})
+    if(query.length == 0) {
+        res.status = 401
+        return res.json({
+            Status: 401,
+            Message: "Username or password is invalid"
+        })
+    }
+    const compare = await comparePassword(password, query[0].password)
+    if(!compare){
+        res.status = 401
+        return res.json({
+            Status: 401,
+            Message: "Username or password is invalid"
+        })
+    }
+
+    const id = query[0].id
+    const token = query[0].token
+
+    res.status = 200
+    return res.json({
+        Status: 200,
+        Message: "Successfully retrieved userdata",
+        Descryption : "Userdata retrieved from the database",
+        Content: {
+            username: username,
+            id : id,
+            token : token,
+        }
+    })
+
+
 } 
