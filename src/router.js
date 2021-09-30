f2ca83db203dfed6896348cd120d62ce1aa95e48
Christian Harjuno <harjuno.tian@gmail.com>
2021-10-01 00:05:32 +0700

    Added token update path

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = new express.Router();
-
+const updateRouter = new express.Router();
 //Handle user creation, deletion, and info retrieval.
 router.get("/register", require("./registry/getuserinfo.js"))
 router.post("/register", require("./registry/createuser.js"))
 router.delete("/register", require("./registry/deleteuser.js"))
 
-
+router.use("/update", updateRouter)
+updateRouter.post("/token", require("./update/updatetoken.js"))
 module.exports = router
