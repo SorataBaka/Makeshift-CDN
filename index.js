0b1c95b9e4b9e3d4ff6e0cc4a7ad77bd9bebe03c
Christian Harjuno <harjuno.tian@gmail.com>
2021-10-02 13:02:16 +0700

    Changed env system

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 const app = new express();
 //check if required env's are available
-if(!process.env.URI && !process.env.PORT){
+if(!process.env["URI"] && !process.env['PORT']){
     console.log("Please provide a mongoDB URI and running PORT")
     process.exit(1)
 }
@@ -36,7 +36,7 @@ if(!fs.existsSync("./data") || !fs.existsSync("./data/temp")){
 }
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env["PORT"] || 3000
 app.listen(PORT, async() =>{
     console.log(`Application is now listening on port ${PORT}`)
     await connectMongoose()
